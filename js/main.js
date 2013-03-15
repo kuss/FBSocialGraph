93b0810fd647d17b63e94e3a35ec2658ff2ed26b
Jaeman An <ajmbell@bawi.org>
2013-03-16 03:13:15 +0900

    Facebook 연동 (callback 함수에서 MutualFriends 변경이 불가능해서 Edge가 안그려짐)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-function init() {
-  // Instanciate sigma.js and customize it :
-  console.log("init called");
-  var sigInst = sigma.init(document.getElementById('social-graph')).drawingProperties({
+var Friends = [];
+var clusters = [];
+var numClusters = 5;
+var sigInst;
+
+function initGraph() {
+  sigInst = sigma.init(document.getElementById('social-graph')).drawingProperties({
     defaultLabelColor: '#fff',
 	defaultLabelSize: 14,
     defaultLabelBGColor: '#fff',
@@ -9,77 +12,65 @@ function init() {
     labelThreshold: 6,
     defaultEdgeType: 'curve'
   }).graphProperties({
-      minNodeSize: 0.5,
-      maxNodeSize: 5,
-      minEdgeSize: 1,
-      maxEdgeSize: 1,
+    minNodeSize: 0.5,
+    maxNodeSize: 5,
+    minEdgeSize: 1,
+    maxEdgeSize: 1,
   });
+}
 
-  // Generate a random graph with :
-  //   . N nodes
-  //   . E edges
-  //   . C clusters
-  //   . d the proportion of edges that connect two nodes
-  //     from the same cluster
-  var i, N = 500, E = 3000, C = 5, d = 0.5, clusters = [];
-  for(i = 0; i < C; i++){
-    clusters.push({
-      'id': i,
-      'nodes': [],
-      'color': 'rgb('+Math.round(Math.random()*256)+','+
-                      Math.round(Math.random()*256)+','+
-                      Math.round(Math.random()*256)+')'
-    });
-  }
+function generateNode() {
+	for(i = 0; i < numClusters; i++){
+    	clusters.push({
+	    	'id': i,
+		    'nodes': [],
+		    'color': 'rgb('+Math.round(Math.random()*256)+','+
+        	                Math.round(Math.random()*256)+','+
+              	  	        Math.round(Math.random()*256)+')'
+	    });
+  	}
 
-  for(i = 0; i < N; i++){
-    var cluster = clusters[(Math.random()*C)|0];
-    sigInst.addNode('n'+i,{
-      'x': Math.random(),
-      'y': Math.random(),
-      'size': 0.5+4.5*Math.random(),
-      'color': cluster['color'],
-      'cluster': cluster['id']
-    });
-    cluster.nodes.push('n'+i);
-  }
+	for (friend in Friends) {
+    	var cluster = clusters[(Math.random()*numClusters)|0];
+		sigInst.addNode(friend, {
+			'x': Math.random(),
+			'y': Math.random(),
+			'size': 0.5 + 0.05 * Friends[friend]['numMutuals'],
+      		'color': cluster['color'],
+		    'cluster': cluster['id'],
+			'label': Friends[friend]['name']
+		});
+    	cluster.nodes.push('n'+i);
+	}
 
-  for(i = 0; i < E; i++){
-    if(Math.random() < 1-d){
-      sigInst.addEdge(i,'n'+(Math.random()*N|0),'n'+(Math.random()*N|0));
-    }else{
-      var cluster = clusters[(Math.random()*C)|0], n = cluster.nodes.length;
-      sigInst.addEdge(i,cluster.nodes[Math.random()*n|0],cluster.nodes[Math.random()*n|0]);
-    }
-  }
+	var numEdge = 0;
+	for (friend in Friends) {
+		for (mutual in Friends[friend]['mutuals']) {
+			sigInst.addEdge(numEdge++, friend, Friends[friend]['mutuals'][mutual]["id"]);
+		}
+	}
+}
 
-  // Start the ForceAtlas2 algorithm
-  // (requires "sigma.forceatlas2.js" to be included)
-      var newParent = document.getElementById('mouselayer-sigma-1');
-	      var mouseLayer = document.getElementById('sigma_mouse_1');
-      mouseLayer.addEventListener('mouseover', function() {
-		        sigInst.activateFishEye();
-				    }, true);
-	      mouseLayer.addEventListener('mouseout', function() {
-			        sigInst.desactivateFishEye().draw(2,2,2);
-					    }, true);
+function clustering() {
+}
 
-  sigInst.startForceAtlas2();
+function Main() {
+	initGraph();
+	generateNode();
+	clustering();
+	draw();
+}
 
-  /*
-  var isRunning = true;
-  document.getElementById('stop-layout').addEventListener('click',function(){
-    if(isRunning){
-      isRunning = false;
-      sigInst.stopForceAtlas2();
-      document.getElementById('stop-layout').childNodes[0].nodeValue = 'Start Layout';
-    }else{
-      isRunning = true;
-      sigInst.startForceAtlas2();
-      document.getElementById('stop-layout').childNodes[0].nodeValue = 'Stop Layout';
-    }
-  },true);
-  document.getElementById('rescale-graph').addEventListener('click',function(){
-    sigInst.position(0,0,1).draw();
-  },true);*/
+function draw() {
+	sigInst.draw();
+	/*
+	var newParent = document.getElementById('mouselayer-sigma-1');
+	var mouseLayer = document.getElementById('sigma_mouse_1');
+    mouseLayer.addEventListener('mouseover', function() {
+		sigInst.activateFishEye();
+	}, true);
+	mouseLayer.addEventListener('mouseout', function() {
+	    sigInst.desactivateFishEye().draw(2,2,2);
+	}, true);
+	sigInst.startForceAtlas2();*/
 }
